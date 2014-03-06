590a456682c3b55e589093ff38acca2c25ebd530
skozawa <shunsuke.kozawa@gmail.com>
2014-03-07 00:51:13 +0900

    add sqlite3 and perl to setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -4,103 +4,176 @@ if [ ! -d tools ]; then
     mkdir tools
 fi
 
+# The tools are not installed if you use the following variables
+# NO_PERL=1, NO_CRF=1, NO_SVM_TOOL=1, NO_YAMCHA=1
+# NO_MECAB=1, NO_MECAB_DIC=1, NO_SQLITE=1, NO_UNIDIC_DB=1
+
+## Default Path
 PERL=$(which perl)
 JAVA=$(which java)
+CRF=/usr/local/bin
+YAMCHA=/usr/local/bin
+MECAB=/usr/local/bin
+MECAB_DIC=/usr/local/lib/mecab/dic
+UNIDIC_DB=/usr/local/unidic2/share/unidic.db
 
 PREFIX=$(pwd)/tools
 
-# CRF++
-if [ ! -x tools/bin/crf_test ]; then
-    echo "*** GET CRF++ ***"
-    wget https://crfpp.googlecode.com/files/CRF%2B%2B-0.58.tar.gz
-    echo "*** INSTALL CRF++ ***"
-    tar -xzf CRF++-0.58.tar.gz
-    cd CRF++-0.58
-    ./configure --prefix=${PREFIX}
-    make
-    make install
-    cd ..
-    rm -rf CRF++-0.58
-    rm -f CRF++-0.58.tar.gz
-    echo "*** INSTALL DONE CRF++ ***"
+## CRF++
+if [ -z $NO_CRF ]; then
+    CRF=${PREFIX}/bin
+    if [ ! -x ${PREFIX}/bin/crf_test ]; then
+        echo "*** GET CRF++ ***"
+        wget https://crfpp.googlecode.com/files/CRF%2B%2B-0.58.tar.gz
+        echo "*** INSTALL CRF++ ***"
+        tar -xzf CRF++-0.58.tar.gz
+        cd CRF++-0.58
+        ./configure --prefix=${PREFIX}
+        make
+        make install
+        cd ..
+        rm -rf CRF++-0.58
+        rm -f CRF++-0.58.tar.gz
+        echo "*** INSTALL DONE CRF++ ***"
+    fi
 fi
 
-# TinySVM
-if [ ! -x tools/bin/svm_classify ]; then
-    echo "*** GET TinySVM ***"
-    wget http://chasen.org/~taku/software/TinySVM/src/TinySVM-0.09.tar.gz
-    echo "*** INSTALL TinySVM ***"
-    tar -xzf TinySVM-0.09.tar.gz
-    cd TinySVM-0.09
-    ./configure --prefix=${PREFIX}
-    make
-    make install
-    cd ..
-    rm -rf TinySVM-0.09
-    rm -f TinySVM-0.09.tar.gz
-    echo "*** INSTALL DONE TinySVM ***"
+## TinySVM
+if [ -z $NO_SVM_TOOL ]; then
+    SVM_TOOL=${PREFIX}/bin
+    if [ ! -x ${PREFIX}/bin/svm_classify ]; then
+        echo "*** GET TinySVM ***"
+        wget http://chasen.org/~taku/software/TinySVM/src/TinySVM-0.09.tar.gz
+        echo "*** INSTALL TinySVM ***"
+        tar -xzf TinySVM-0.09.tar.gz
+        cd TinySVM-0.09
+        ./configure --prefix=${PREFIX}
+        make
+        make install
+        cd ..
+        rm -rf TinySVM-0.09
+        rm -f TinySVM-0.09.tar.gz
+        echo "*** INSTALL DONE TinySVM ***"
+    fi
 fi
 
-# yamcha
-if [ ! -x tools/bin/yamcha ]; then
-    echo "*** GET yamcha ***"
-    wget http://chasen.org/~taku/software/yamcha/src/yamcha-0.33.tar.gz
-    echo "*** INSTALL yamcha ***"
-    tar -xzf yamcha-0.33.tar.gz
-    cd yamcha-0.33
-    ./configure --prefix=${PREFIX} --with-svm-learn=${PREFIX}
-    make
-    make install
-    cd ..
-    if [ ! -x tools/bin/yamcha ]; then
-        echo "*** APPLY Yamcha Patch for INSTALL Error ***"
-        wget http://unicus.jp/pub/yamcha.patch
-        patch -p0 < yamcha.patch
+# Yamcha
+if [ -z $NO_YAMCHA ]; then
+    YAMCHA=${PREFIX}/bin
+    if [ ! -x ${PREFIX}/bin/yamcha ]; then
+        echo "*** GET yamcha ***"
+        wget http://chasen.org/~taku/software/yamcha/src/yamcha-0.33.tar.gz
+        echo "*** INSTALL yamcha ***"
+        tar -xzf yamcha-0.33.tar.gz
         cd yamcha-0.33
         ./configure --prefix=${PREFIX} --with-svm-learn=${PREFIX}
         make
         make install
         cd ..
-        rm yamcha.patch
+        if [ ! -x ${PREFIX}/bin/yamcha ]; then
+            echo "*** APPLY Yamcha Patch for INSTALL Error ***"
+            wget http://unicus.jp/pub/yamcha.patch
+            patch -p0 < yamcha.patch
+            cd yamcha-0.33
+            ./configure --prefix=${PREFIX} --with-svm-learn=${PREFIX}
+            make
+            make install
+            cd ..
+            rm yamcha.patch
+        fi
+        rm -rf yamcha-0.33
+        rm -f yamcha-0.33.tar.gz
+        echo "*** INSTALL DONE yamcha ***"
     fi
-    rm -rf yamcha-0.33
-    rm -f yamcha-0.33.tar.gz
-    echo "*** INSTALL DONE yamcha ***"
 fi
 
 # MeCab
-if [ ! -x tools/bin/mecab ]; then
-    echo "*** GET MeCab ***"
-    wget https://mecab.googlecode.com/files/mecab-0.996.tar.gz
-    echo "*** INSTALL MeCab ***"
-    tar -xzf mecab-0.996.tar.gz
-    cd mecab-0.996
-    ./configure --prefix=${PREFIX}
-    make
-    make install
-    cd ..
-    rm -rf mecab-0.996
-    rm -f mecab-0.996.tar.gz
-    echo "*** INSTALL DONE MeCab ***"
+if [ -z $NO_MECAB ]; then
+    MECAB=${PREFIX}/bin
+    if [ ! -x ${PREFIX}/bin/mecab ]; then
+        echo "*** GET MeCab ***"
+        wget https://mecab.googlecode.com/files/mecab-0.996.tar.gz
+        echo "*** INSTALL MeCab ***"
+        tar -xzf mecab-0.996.tar.gz
+        cd mecab-0.996
+        ./configure --prefix=${PREFIX}
+        make
+        make install
+        cd ..
+        rm -rf mecab-0.996
+        rm -f mecab-0.996.tar.gz
+        echo "*** INSTALL DONE MeCab ***"
+    fi
 fi
 
 # MeCab-UniDic
-if [ ! -d tools/lib/mecab/dic ]; then
-    echo "*** GET MeCab-UniDic ***"
-    wget "http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Funidic%2F58338%2Funidic-mecab-2.1.2_src.zip"
-    echo "*** INSTALL MeCab-UniDic ***"
-    unzip unidic-mecab-2.1.2_src.zip
-    cd unidic-mecab-2.1.2_src
-    MECAB_CONFIG=${PREFIX}/bin/mecab-config ./configure --prefix=${PREFIX}
-    make
-    make install
-    cd ..
-    rm -rf unidic-mecab-2.1.2_src
-    rm -f unidic-mecab-2.1.2_src.zip
-    echo "*** INSTALL DONE MeCab-Unidic ***"
+if [ -z $NO_MECAB_DIC ]; then
+    MECAB_DIC=${PREFIX}/lib/mecab/dic
+    if [ ! -d ${PREFIX}/lib/mecab/dic ]; then
+        echo "*** GET MeCab-UniDic ***"
+        wget "http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Funidic%2F58338%2Funidic-mecab-2.1.2_src.zip"
+        echo "*** INSTALL MeCab-UniDic ***"
+        unzip unidic-mecab-2.1.2_src.zip
+        cd unidic-mecab-2.1.2_src
+        MECAB_CONFIG=${PREFIX}/bin/mecab-config ./configure --prefix=${PREFIX}
+        make
+        make install
+        cd ..
+        rm -rf unidic-mecab-2.1.2_src
+        rm -f unidic-mecab-2.1.2_src.zip
+        echo "*** INSTALL DONE MeCab-Unidic ***"
+    fi
+fi
+
+## SQLITE3
+if [ -z $NO_SQLITE ]; then
+    if [ ! -x ${PREFIX}/bin/sqlite3 ]; then
+        echo "*** GET SQLITE3 ***"
+        wget "http://www.sqlite.org/2014/sqlite-autoconf-3080301.tar.gz"
+        tar -xzf sqlite-autoconf-3080301.tar.gz
+        cd sqlite-autoconf-3080301
+        ./configure --prefix=${PREFIX}
+        make
+        make install
+        cd ..
+        rm -rf sqlite-autoconf-3080301
+        rm -f sqlite-autoconf-3080301.tar.gz
+    fi
+fi
+
+## Unidic DB (Unidic2)
+if [ -z $NO_UNIDIC_DB ]; then
+    UNIDIC_DB=${PREFIX}
+#    if [ ! -f ${PREFIX}/share/unidic.db ]; then
+# wget unidic-tool
+# unzip unidic-tool.zip
+# cd unidic-tool
+# wget unidic-xml
+# unzip unidic-xml.zip
+# mv unidic-xml xml
+# wget unidic-tool.patch
+# patch -u configure < unidic-tool.patch
+## ${PREFIX}/bin/sqlite3 ./configure --with-dbfile= --with-deffile=config/core.def --prefix=${PREFIX}
+# ./configure --with-dbfile=${PREFIX}/share/unidic.db --with-deffile=config/core.def --prefix=${PREFIX}
+# sed -i "s/xmldir=\/usr\/local\/unidic2/xmldir=\/root\/Comainu\/unidic-tools/" config/core.def
+# C_INCLUDE_PATH=${PREFIX}/include make
+# make install
+#    fi
 fi
 
 
-./configure --perl=${PERL} --java=${JAVA} --crf-dir=${PREFIX}/bin \
-    --svm-tool-dir=${PREFIX}/bin --yamcha-dir=${PREFIX}/bin \
-    --mecab-dir=${PREFIX}/bin --mecab-dic-dir=${PREFIX}/lib/mecab/dic
+# PERL
+if [ -z $NO_PERL ]; then
+    PERL_VERSION=5.18.2
+    PERL=${PREFIX}/opt/perl-${PERL_VERSION}/bin/perl
+    echo "*** INSTALL PERL ***"
+    curl https://raw.github.com/tokuhirom/Perl-Build/master/perl-build | perl - ${PERL_VERSION} ${PREFIX}/opt/perl-${PERL_VERSION}/
+    echo "*** INSTALL CPANM ***"
+    curl -L http://cpanmin.us | ${PREFIX}/opt/perl-${PERL_VERSION}/bin/perl - App::cpanminus
+    echo "*** INSTALL PERL MODULE ***"
+    ${PREFIX}/opt/perl-${PERL_VERSION}/bin/cpanm install DBI DBD::SQLite
+fi
+
+./configure --perl=${PERL} --java=${JAVA} --crf-dir=${CRF} \
+    --svm-tool-dir=${SVM_TOOL} --yamcha-dir=${YAMCHA}/bin \
+    --mecab-dir=${MECAB} --mecab-dic-dir=${MECAB_DIC} --unidic-db=${UNIDIC_DB}
